2d87d557243829db4afcf44d49def6dfad62c538
Sygeman <sygeman@gmail.com>
2023-11-26 21:22:37 +0300

    rm body style

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t3p0pvs12s"
+    "revision": "0.ntd6bv4nedo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,14 @@
-import { ImportDataModal } from "./import-data/modal";
+import data from "../data/641f24449a150cba09f92d5b.json";
 
 export const App = () => {
+  console.log(data);
+
   return (
-    <>
-      <ImportDataModal />
-    </>
+    <div className="insert-0 h-full w-full absolute">
+      <div className="flex w-full h-12 justify-center items-center">
+        {data.title}
+      </div>
+      <div className="w-full h-[calc(100%-48px)]">Tree</div>
+    </div>
   );
 };
